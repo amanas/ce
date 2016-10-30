be2934bd241daf7ccbeced1793e1bf09ca3c2cf5
amanas <andres.manas.manas@gmail.com>
2016-10-30 13:28:17 +0100

    a commit

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject ce "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "FIXME: write d escription"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
