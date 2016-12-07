7e159db64ea0ff2afadeca481e917532fe3b9feb
amanas <andresmanasmanas@gmail.com>
2016-12-07 10:59:47 +0000

    auto-push

---
+++ www/service.php
@@ -0,0 +1,22 @@
+<?php 
+    
+# apc_delete ("amanas: experimento 1");
+
+if ($_GET["action"] == "post") {
+    apc_store($_GET["name"], file_get_contents('php://input'), 0);
+}
+
+if ($_GET["action"] == "list") {
+    $result = array();
+    $iter = new APCIterator('user');
+    foreach ($iter as $item) {
+        array_push($result, $item['key']);
+    }
+    echo json_encode($result);
+}
+
+if ($_GET["action"] == "get") {
+    echo apc_fetch($_GET["name"]);
+}
+
+?>
